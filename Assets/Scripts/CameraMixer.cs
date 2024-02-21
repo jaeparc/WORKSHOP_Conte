2b5f53a8f7b066f7218a86e8cecb2fbdc0ca3b3d
Nayl0rs <naylors.m@gmail.com>
2024-02-21 17:24:58 +0100

    test camera cinemachine
    pas fini haha au cas où quoi ^^

---
+++ Assets/Scripts/CameraMixer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMixer : MonoBehaviour
+{
+    public GameObject mixingCamera;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider hit)
+    {
+        if (hit.tag == "Player")
+        {
+            mixingCamera.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider hit)
+    {
+        if (hit.tag == "Player")
+        {
+            mixingCamera.SetActive(false);
+        }
+    }
+}
