9bec1b2beed113d183da61c47ef4f96b1e92da06
Jsuitonper <rafab0603@gmail.com>
2024-02-22 17:10:12 +0100

    Debut Travail  Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     private Vector3 velocity;
     private float groundSnapForce = -2f; // Additional downward force to improve slope handling
     public bool isGrounded;
+    public GameObject RayBas, RayHaut;
     private void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -35,7 +36,12 @@ public class PlayerController : MonoBehaviour
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+
+        ClimbCheck();
+
+
     }
+
 
     void Walk()
     {
@@ -54,7 +60,7 @@ public class PlayerController : MonoBehaviour
         if (moveDirection != Vector3.zero)
         {
             Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, speed * Time.deltaTime * 20);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, speed * Time.deltaTime * 200);
 
             // Adjust movement based on slope
             if (isGrounded)
@@ -73,5 +79,40 @@ public class PlayerController : MonoBehaviour
     {
         // Apply a more significant jump force to counteract increased gravity
         velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+    }
+
+    void ClimbCheck()
+    {
+        Vector3 RayBasStart = RayBas.transform.position;
+        Vector3 RayHautStart = RayHaut.transform.position;
+        Vector3 RayDirection = transform.forward;
+
+        float MaxDistance = 0.1f;
+
+        RaycastHit hit;
+
+        RaycastHit hit2;
+        float MaxDistance2 = 0.5f;
+
+        if (Physics.Raycast(RayBasStart, RayDirection, out hit, MaxDistance))
+        {
+            if (Mathf.Abs(hit.normal.y) < 0.5f)
+            {
+
+
+                if (Physics.Raycast(RayHautStart, RayDirection, out hit2, MaxDistance2))
+                {
+                    
+                }
+                else
+                {
+                    transform.position = new Vector3(transform.position.x, transform.position.y+0.5f,transform.position.z);
+                }
+
+            }
+        }
+
+        Debug.DrawRay(RayBasStart, RayDirection * MaxDistance, Color.red);
+        Debug.DrawRay(RayHautStart, RayDirection * MaxDistance2, Color.red);
     }
 }
