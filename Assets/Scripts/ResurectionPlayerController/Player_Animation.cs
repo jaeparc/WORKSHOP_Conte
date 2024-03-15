ed3b9c1d747b015175e4f7c53fcb5a68412a68f6
Jsuitonper <rafab0603@gmail.com>
2024-03-15 15:00:21 +0100

    push de bz

--- Assets/Scripts/ResurectionPlayerController/Player_Animation.cs
+++ Assets/Scripts/ResurectionPlayerController/Player_Animation.cs
@@ -6,34 +6,38 @@ public class Player_Animation : Player_Component
 {
 
     public Animator anim;
-    
+
     public override void UpdatePlayer(PlayerController player, CharacterController controller)
     {
-        bool canJump = true;
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
-        Vector3 Movement = (moveZ * Vector3.up + moveX * Vector3.right);
-        if (Movement != Vector3.zero)
-        {
-            anim.SetBool("IsMoving", true);
-        }
-        else
-        {
-            anim.SetBool("IsMoving", false);
-        }
+        /*        bool canJump = true;
+                float moveX = Input.GetAxis("Horizontal");
+                float moveZ = Input.GetAxis("Vertical");
+                Vector3 Movement = (moveZ * Vector3.up + moveX * Vector3.right);
+                if (Movement != Vector3.zero)
+                {
+                    anim.SetBool("IsMoving", true);
+                }
+                else
+                {
+                    anim.SetBool("IsMoving", false);
+                }
 
 
-        if (canJump && Input.GetButtonDown("Fire1")) 
-        {
-            anim.SetBool("Jump", true);
-        }
+                if (canJump && Input.GetButtonDown("Fire1")) 
+                {
+                    anim.SetBool("Jump", true);
+                }
 
-        if (controller.isGrounded)
-        {
-            anim.SetBool("Jump", false);
-        }
+                if (controller.isGrounded)
+                {
+                    anim.SetBool("Jump", false);
+                }
 
-        anim.SetBool("isGrounded", controller.isGrounded);
+                anim.SetBool("isGrounded", controller.isGrounded);
+            */
+
+        anim.SetFloat("XZvelocity", player.velocity.x + player.velocity.z);
+        anim.SetFloat("Yvelocity", player.velocity.y);
     }
 }
 
